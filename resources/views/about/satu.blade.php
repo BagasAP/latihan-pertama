6dc86df29f499c797195c45d3427bbb4732861f4
RahmadFirdaus <151610242@smkassalaambandung.sch.id>
2017-07-27 16:48:33 +0700

    laravel

---
+++ resources/views/about/satu.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman about 1</title>
+</head>
+<body>
+Halaman about 1
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,37 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about',function() {
+	return '<h1>Halo</h1>'
+	.'selamat datang di web saya <br>'
+	.'laravel, emang keren.';
+});
+
+Route::get('/awal',function() {
+	return view('about');
+});
+
+Route::get('about/1',function(){
+	
+	return view('about.satu');
+});
+
+Route::get('about/2',function(){
+	return view('about.dua');
+});
+
+Route::get('about/3',function(){
+	return view('about.tiga');
+});
+
+Route::get('siswa/{nama}/{sekolah}/{umur}',function($a,$b,$c){
+	return 'Nama Saya :'.$a.'<br>' 
+			.'Sekolah :'.$b.'<br>'
+			.'Umur :'.$c;
+});
+
+Route::get('nama/{nama}',function($a){
+	$a='John';
+	return 'Ini halaman about '.$a;
+});
